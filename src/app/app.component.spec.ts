860e21fde9daa378cd175e23273c63df9ec0cc6a
armel <armelkepjio@yahoo.fr>
2021-01-03 14:11:24 +0000

    modify app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'stickerz'`, () => {
+  it(`should have as title 'Devopski'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('stickerz');
+    expect(app.title).toEqual('Devopski');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('stickerz app is running!');
+    expect(compiled.querySelector('.content span')).toContain('stickerz app is running!');
   });
 });
